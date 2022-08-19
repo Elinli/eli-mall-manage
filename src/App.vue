a21c6763199bad650bcfbf73a668768c384314c1
shixx <shixiaoxiao@cechealth.cn>
2022-08-19 15:04:54 +0800

    build: provider

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="app-container">
-    <!-- <el-config-provider :locale="locale"> -->
-    <router-view />
-    <!-- </el-config-provider> -->
+    <el-config-provider :locale="locale">
+      <router-view />
+    </el-config-provider>
   </div>
 </template>
 
 <script lang="ts" setup>
-  // import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
-  // import en from 'element-plus/dist/locale/en.mjs'
-  // import { useLocale } from '/@/store/locale'
-  // const localeStore = useLocale()
-  // const locale = computed(() => (localeStore.getLocale.toLowerCase() === 'zh-cn' ? zhCn : en))
+  import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+  import en from 'element-plus/dist/locale/en.mjs'
+  import { useLocale } from '/@/store/locale'
+  const localeStore = useLocale()
+  const locale = computed(() => (localeStore.getLocale.toLowerCase() === 'zh-cn' ? zhCn : en))
 </script>
