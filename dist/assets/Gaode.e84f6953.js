69e349a4dde82864934be8369a6fab5069548c48
shixx <shixiaoxiao@cechealth.cn>
2022-09-16 14:10:34 +0800

    ci: delete dist files

--- dist/assets/Gaode.e84f6953.js
+++
@@ -1,8 +0,0 @@
-import { _ as e } from './_plugin-vue_export-helper.cdc0426e.js'
-import { o, c } from './index.3ffc5a30.js'
-const r = {}
-function t(n, a) {
-  return o(), c('div', null, 'gaode')
-}
-const f = e(r, [['render', t]])
-export { f as default }
--- dist/assets/TopBarNav.53e949af.js
+++
@@ -1,4 +0,0 @@
-import './TopBarNav.vue_vue_type_style_index_0_lang.029c5067.js'
-import { _ as t } from './TopBarNav.vue_vue_type_style_index_0_lang.029c5067.js'
-import './index.3ffc5a30.js'
-export { t as default }
