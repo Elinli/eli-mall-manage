2c4e0ad361f9210fb8d1f40359cd396b74837ee1
shixx <shixiaoxiao@cechealth.cn>
2022-09-16 11:34:33 +0800

    build: refresh webpage

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,10 @@
 <template>
-  <div> {{ a }}{{ b }} </div>
+  <div> {{ a }} </div>
 </template>
 
 <script lang="ts" setup>
-  import { reactive, ref } from 'vue'
+  import { reactive } from 'vue'
   const a = reactive([])
-  const b = ref(1)
 </script>
 
 <style scoped lang="scss"></style>
