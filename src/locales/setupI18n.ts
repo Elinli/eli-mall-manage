b1ea73b07651cd1bdd81cc93ba597638aeee3004
shixx <shixiaoxiao@cechealth.cn>
2022-09-22 17:13:18 +0800

    cicd: deploy error

--- src/locales/setupI18n.ts
+++ src/locales/setupI18n.ts
@@ -29,7 +29,7 @@ async function createI18nOptions(): Promise<I18nOptions> {
 }
 
 // setup i18n instance with glob
-// ts-ingore
+// @ts-ignore
 export async function setupI18n(app: App) {
   const options = await createI18nOptions()
   i18n = createI18n(options) as unknown as I18n
